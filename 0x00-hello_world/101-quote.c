cfe19426edc9759de382a62a7d066ed186a8f675
Nonso024 <josephani772@gmail.com>
2022-10-13 13:23:04 -0700

    another

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-	char err[] = "and that piece of art is\" - Dora Korpar, 2015-10-1
-9\n";
-	write(2, err, 59);
+	write(2,
+
+						"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+									59);
 
 	return (1);
 }
