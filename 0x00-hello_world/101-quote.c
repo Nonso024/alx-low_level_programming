41198c782aac28e526443cb44bf398027746611f
Nonso024 <josephani772@gmail.com>
2022-10-13 13:19:25 -0700

    trail

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-	write(3,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	char err[] = "and that piece of art is\" - Dora Korpar, 2015-10-1
+9\n";
+	write(2, err, 59);
+
 	return (1);
 }
