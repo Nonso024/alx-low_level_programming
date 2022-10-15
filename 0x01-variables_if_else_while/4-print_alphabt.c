15aa7d1346ce276156882ac412134c0c09860eeb
Nonso024 <josephani772@gmail.com>
2022-10-15 09:36:45 -0700

    print alpha 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		if (low == 'q' || low == 'e')
+			continue;
+				putchar(low);
+	putchar('\n');
+	return (0);
+}
