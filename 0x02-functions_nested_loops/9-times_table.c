1f3b5742841e3946aa56e357b483ebf5643f305b
Nonso024 <josephani772@gmail.com>
2022-10-19 02:51:43 -0700

    timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,28 @@ void times_table(void)
 {
 	int row, column, product;
 
-	for (row = 0; row <= 9; row++)
+	for (row = 0; row <= 10; row++)
 	{
-		_putchar(0);
 		for (column = 0; column <= 9; column++)
 		{
-			_putchar(',');
-			_putchar(' ');
 			product = row * column;
-			if (product <= 9)
+			if (column == 0)
+			{
+				_putchar(column + '0');
+			}
+			if (product < 10 && column != 0)
+			{
+				_putchar(',');
 				_putchar(' ');
-			else
+				_putchar(' ');
+				_putchar(product + '0');
+			} else if (product >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
