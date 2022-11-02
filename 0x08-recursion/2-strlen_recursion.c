ec5258f537160a127962f01b5cb2e5d903799286
Nonso024 <josephani772@gmail.com>
2022-11-02 06:12:33 -0700

    str legnth

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ * Return: Length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int count;
+
+	if (*s == '\0')
+		return (0);
+	count = 1 + _strlen_recursion(s + 1);
+
+	return (count);
+}
